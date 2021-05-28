557f74d35a1dbd046b1ffed172652672c56c8c93
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-05-28 15:34:13 +0200

    Remove code duplication by using helper macro

--- src/impls.rs
+++ src/impls.rs
@@ -84,6 +84,18 @@ use std::convert::TryFrom;
 use crate::{Literal, err::{InvalidToken, TokenKind}};
 
 
+/// Helper macro to call a `callback` macro four times for all combinations of
+/// `proc_macro`/`proc_macro2` and `&`/owned.
+macro_rules! helper {
+    ($callback:ident, $($input:tt)*) => {
+        $callback!([] [proc_macro::] => $($input)*);
+        $callback!([] [&proc_macro::] => $($input)*);
+        $callback!([#[cfg(feature = "proc-macro2")]] [proc_macro2::] => $($input)*);
+        $callback!([#[cfg(feature = "proc-macro2")]] [&proc_macro2::] => $($input)*);
+    };
+}
+
+
 // ==============================================================================================
 // ===== `From<*Lit> for Literal`
 // ==============================================================================================
@@ -112,108 +124,57 @@ impl_specific_lit_to_lit!(crate::ByteStringLit<B>, ByteString);
 // ===== `From<pm::Literal> for Literal`
 // ==============================================================================================
 
-// We call `expect` in all these impls: this library aims to implement exactly
-// the Rust grammar, so if we have a valid Rust literal, we should always be
-// able to parse it.
-impl From<proc_macro::Literal> for Literal<String> {
-    fn from(src: proc_macro::Literal) -> Self {
-        Self::parse(src.to_string())
-            .expect("bug: failed to parse output of `Literal::to_string`")
+
+macro_rules! impl_tt_to_lit {
+    ([$($cfg:tt)*] [$($prefix:tt)*] => ) => {
+        $($cfg)*
+        impl From<$($prefix)* Literal> for Literal<String> {
+            fn from(src: $($prefix)* Literal) -> Self {
+                // We call `expect` in all these impls: this library aims to implement exactly
+                // the Rust grammar, so if we have a valid Rust literal, we should always be
+                // able to parse it.
+                Self::parse(src.to_string())
+                    .expect("bug: failed to parse output of `Literal::to_string`")
+            }
+        }
     }
 }
 
-impl From<&proc_macro::Literal> for Literal<String> {
-    fn from(src: &proc_macro::Literal) -> Self {
-        Self::parse(src.to_string())
-            .expect("bug: failed to parse output of `Literal::to_string`")
-    }
-}
-
-#[cfg(feature = "proc-macro2")]
-impl From<proc_macro2::Literal> for Literal<String> {
-    fn from(src: proc_macro2::Literal) -> Self {
-        Self::parse(src.to_string())
-            .expect("bug: failed to parse output of `Literal::to_string`")
-    }
-}
-
-#[cfg(feature = "proc-macro2")]
-impl From<&proc_macro2::Literal> for Literal<String> {
-    fn from(src: &proc_macro2::Literal) -> Self {
-        Self::parse(src.to_string())
-            .expect("bug: failed to parse output of `Literal::to_string`")
-    }
-}
+helper!(impl_tt_to_lit, );
 
 
 // ==============================================================================================
 // ===== `TryFrom<pm::TokenTree> for Literal`
 // ==============================================================================================
 
-macro_rules! lit_or_err {
-    ($tt:expr, $krate:ident) => {
-        match $tt {
-            $krate::TokenTree::Group(_) => Err(TokenKind::Group),
-            $krate::TokenTree::Ident(_) => Err(TokenKind::Ident),
-            $krate::TokenTree::Punct(_) => Err(TokenKind::Punct),
-            $krate::TokenTree::Literal(lit) => Ok(lit),
+macro_rules! impl_tt_to_lit {
+    ([$($cfg:tt)*] [$($prefix:tt)*] => ) => {
+        $($cfg)*
+        impl TryFrom<$($prefix)* TokenTree> for Literal<String> {
+            type Error = InvalidToken;
+            fn try_from(tt: $($prefix)* TokenTree) -> Result<Self, Self::Error> {
+                let span = tt.span();
+                let res = match tt {
+                    $($prefix)* TokenTree::Group(_) => Err(TokenKind::Group),
+                    $($prefix)* TokenTree::Ident(_) => Err(TokenKind::Ident),
+                    $($prefix)* TokenTree::Punct(_) => Err(TokenKind::Punct),
+                    $($prefix)* TokenTree::Literal(ref lit) => Ok(lit),
+                };
+
+                match res {
+                    Ok(lit) => Ok(From::from(lit)),
+                    Err(actual) => Err(InvalidToken {
+                        actual,
+                        expected: TokenKind::Literal,
+                        span: span.into(),
+                    }),
+                }
+            }
         }
-    };
-}
-
-impl TryFrom<proc_macro::TokenTree> for Literal<String> {
-    type Error = InvalidToken;
-    fn try_from(tt: proc_macro::TokenTree) -> Result<Self, Self::Error> {
-        lit_or_err!(&tt, proc_macro)
-            .map_err(|actual|  InvalidToken {
-                actual,
-                expected: TokenKind::Literal,
-                span: tt.span().into(),
-            })
-            .map(From::from)
     }
 }
 
-impl TryFrom<&proc_macro::TokenTree> for Literal<String> {
-    type Error = InvalidToken;
-    fn try_from(tt: &proc_macro::TokenTree) -> Result<Self, Self::Error> {
-        lit_or_err!(&tt, proc_macro)
-            .map_err(|actual|  InvalidToken {
-                actual,
-                expected: TokenKind::Literal,
-                span: tt.span().into(),
-            })
-            .map(From::from)
-    }
-}
-
-#[cfg(feature = "proc-macro2")]
-impl TryFrom<proc_macro2::TokenTree> for Literal<String> {
-    type Error = InvalidToken;
-    fn try_from(tt: proc_macro2::TokenTree) -> Result<Self, Self::Error> {
-        lit_or_err!(&tt, proc_macro2)
-            .map_err(|actual|  InvalidToken {
-                actual,
-                expected: TokenKind::Literal,
-                span: tt.span().into(),
-            })
-            .map(From::from)
-    }
-}
-
-#[cfg(feature = "proc-macro2")]
-impl TryFrom<&proc_macro2::TokenTree> for Literal<String> {
-    type Error = InvalidToken;
-    fn try_from(tt: &proc_macro2::TokenTree) -> Result<Self, Self::Error> {
-        lit_or_err!(&tt, proc_macro2)
-            .map_err(|actual|  InvalidToken {
-                actual,
-                expected: TokenKind::Literal,
-                span: tt.span().into(),
-            })
-            .map(From::from)
-    }
-}
+helper!(impl_tt_to_lit, );
 
 
 // ==============================================================================================
@@ -230,15 +191,6 @@ fn kind_of(lit: &Literal<String>) -> TokenKind {
         Literal::Byte(_) => TokenKind::ByteLit,
         Literal::ByteString(_) => TokenKind::ByteStringLit,
     }
-}
-
-macro_rules! helper {
-    ($callback:ident, $($input:tt)*) => {
-        $callback!([] [proc_macro::] => $($input)*);
-        $callback!([] [&proc_macro::] => $($input)*);
-        $callback!([#[cfg(feature = "proc-macro2")]] [proc_macro2::] => $($input)*);
-        $callback!([#[cfg(feature = "proc-macro2")]] [&proc_macro2::] => $($input)*);
-    };
 }
 
 macro_rules! impl_for_specific_lit {
